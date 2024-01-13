c46c9938e3b880a734f7762f9e79e05aab803a51
Izabel Soares <izabelcssoares@gmail.com>
2024-01-13 20:20:17 -0300

    menu dropdown to be fixed

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function drop_function() {
     document.getElementById("dropdown").classList.toggle("show");
+    document.getElementById("dropdown_2").classList.toggle("show");
 }
 
 window.onclick = function(e) {
     if(!e.target.matches(".dropdown_menu_button")) {
         var dropdown = document.getElementById("dropdown");
-        if(dropdown.classList.contains("show")) {
+        var dropdown_2 = document.getElementById("dropdown_2");
+        if((dropdown.classList.contains("show")) || (dropdown_2.classList.contains("show"))) {
             dropdown.classList.remove("show");
         }
     }

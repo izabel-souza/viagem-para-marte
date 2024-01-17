a8a3f1af1379946c6b281f540668c7c4cd2483cf
Izabel Soares <izabelcssoares@gmail.com>
2024-01-17 18:20:33 -0300

    dropdown working and favicon added. dropdown still to be fixed

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-function drop_function() {
-    document.getElementById("dropdown").classList.toggle("show");
-    document.getElementById("dropdown_2").classList.toggle("show");
+function toggleDropdown(dropdownID) {
+    var dropdown = document.getElementById(dropdownID);
+    dropdown.classList.toggle("show");
 }
 
 window.onclick = function(e) {
-    if(!e.target.matches(".dropdown_menu_button")) {
-        var dropdown = document.getElementById("dropdown");
-        var dropdown_2 = document.getElementById("dropdown_2");
-        if((dropdown.classList.contains("show")) || (dropdown_2.classList.contains("show"))) {
-            dropdown.classList.remove("show");
-        }
+    if (!e.target.matches(".dropdown_menu_button")) {
+        var dropdownButtons = document.querySelectorAll(".dropdown_menu_button");
+        dropdownButtons.forEach(function(button) {
+            var dropdownID = button.dataset.dropdown;
+            var dropdown = document.getElementById(dropdownID);
+
+            if (dropdown.classList.contains("show")) {
+                dropdown.classList.remove("show");
+            }
+        });
     }
 }
